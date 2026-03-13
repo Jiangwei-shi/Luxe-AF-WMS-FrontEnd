1daa8e9b16d78114f01cc89a626e81b4bc5f6d5c
Jiangwei-shi <a1006351432@gmail.com>
2026-03-13 14:40:49 +0800

    Readme update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import createVitePlugins from './vite/plugins'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd())
+  const proxyTarget = env.VITE_DEV_PROXY_TARGET || 'http://localhost:8080'
   return {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
@@ -30,8 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'https://backend.luxeafapi.com',
-          // target: 'http://localhost:8080',
+          target: proxyTarget,
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
