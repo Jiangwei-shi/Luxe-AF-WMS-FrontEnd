dcd378a2e78866bf25fb802d73f1c0192a294c98
Jiangwei-shi <a1006351432@gmail.com>
2026-03-22 13:53:44 +0800

    fix phone number issue and hide post id

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -204,7 +204,7 @@
             <el-row>
                <el-col :span="12">
                   <el-form-item label="手机号码" prop="phonenumber">
-                     <el-input v-model="form.phonenumber" placeholder="请输入手机号码" maxlength="11" />
+                     <el-input v-model="form.phonenumber" placeholder="请输入手机号码" />
                   </el-form-item>
                </el-col>
                <el-col :span="12">
@@ -393,8 +393,7 @@ const data = reactive({
     userName: [{ required: true, message: "用户名称不能为空", trigger: "blur" }, { min: 2, max: 20, message: "用户名称长度必须介于 2 和 20 之间", trigger: "blur" }],
     nickName: [{ required: true, message: "用户昵称不能为空", trigger: "blur" }],
     password: [{ required: true, message: "用户密码不能为空", trigger: "blur" }, { min: 5, max: 20, message: "用户密码长度必须介于 5 和 20 之间", trigger: "blur" }],
-    email: [{ type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }],
-    phonenumber: [{ pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }]
+    email: [{ type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }]
   }
 });
 
